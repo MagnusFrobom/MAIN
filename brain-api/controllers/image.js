7f4455592772cd94422e016eafedf61deb68e5fb
Magnus Fröbom <frobom.magnus@gmail.com>
2023-02-02 20:22:15 +0100

    grpc

--- brain-api/controllers/image.js
+++ brain-api/controllers/image.js
@@ -11,18 +11,46 @@ metadata.set("authorization", API_KEY);
 const app = new Clarifai.App({
     apiKey: ''
 }); */
+const handleApiCall = (req, res) => {
+    stub.PostModelOutputs(
+        {
+            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+            model_id: "aaa03c23b3724a16a56b629203edc62c",
+            inputs: [{ data: { image: { url: req.body.input } } }]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
 
+            if (response.status.code !== 10000) {
+                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+                return;
+            }
+
+            console.log("Predicted concepts, with confidence values:")
+            for (const c of response.outputs[0].data.concepts) {
+                console.log(c.name + ": " + c.value);
+            }
+        }
+);
+
+}
+
+/* 
 stub.PostModelOutputs(
         {
             model_id: ""
         }
-)
+) */
 
-const handleApiCall = (req, res) => {
+/* const handleApiCall = (req, res) => {
     app.models
         .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
 
-}
+} */
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
