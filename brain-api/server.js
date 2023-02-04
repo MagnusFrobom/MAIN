9178d640896408e124bb380e289e2b19e50d7a23
Magnus Fröbom <frobom.magnus@gmail.com>
2023-02-04 10:54:07 +0100

    Going for Heroku

--- brain-api/server.js
+++ brain-api/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
-// const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const Clarifai = require('clarifai');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const DB_KEY = process.env.DB_KEY
 const DB_URL = process.env.DB_URL
+
 const db = knex({
   // Enter your database information here
   client: 'pg', // PostGres
@@ -28,22 +27,17 @@ db.select('*').from('users').then(data => {
 const app = express();
 
 app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(express.json());
 
-// Test only - when you have a database variable you want to use
-// app.get('/', (req, res)=> {
-//   res.send(database.users);
-// })
-
-app.get('/', (req, res) => { res.send(db.users)})
+app.get('/', (req, res) => { res.send('it is working')})
 app.post('./controllers/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('./controllers/register.js', (req, res) => {register.handleRegister(req, res, db, bcrypt )})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGET(req, res, db, bcrypt)})
 app.put('/image', (req, res) => { image.handleImage(req ,res)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3001, () => {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3001, () => {
+  console.log('app is running on port ${process.env.PORT}');
 })
 
 
