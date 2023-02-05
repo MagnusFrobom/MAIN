30ee257f40973349e0c7331bb538938347f22ce3
Magnus Fröbom <frobom.magnus@gmail.com>
2023-02-05 20:28:59 +0100

    Cosmetics

--- Brain/src/components/Navigation/Navigation.js
+++ Brain/src/components/Navigation/Navigation.js
@@ -3,18 +3,25 @@ import React from 'react';
 const Navigation = ({ onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
       return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-          <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>SIGN OUT</p>
+        <nav style={{display: 'flex', justifyContent: 'right'}}>
+          <p onClick={() => onRouteChange('signout')} className='f6 link dim ba bw1 ph3 pv2 mb2 dib pointer dark-grey'>SIGN OUT</p>
         </nav>
       );
     } else {
       return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-          <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>SIGN IN</p>
-          <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>REGISTER</p>
+        <nav style={{
+          display: 'flex', justifyContent: 'right'}}>
+          <p onClick={() => onRouteChange('signin')} className='f6 link dim ba bw1 ph3 pv2 mb2 dib pointer dark-grey'>SIGN IN</p>
+          <p onClick={() => onRouteChange('register')} className='f6 link dim ba bw1 ph3 pv2 mb2 dib pointer dark-grey'>REGISTER</p>
         </nav>
       );
     }
 }
 
 export default Navigation;
+
+/* 
+'flex' */
+
+/* 
+'inline-block' */
