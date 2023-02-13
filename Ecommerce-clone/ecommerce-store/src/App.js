6972cf9d906826d2db37fa96ee43655c8f7679bd
Magnus Fröbom <frobom.magnus@gmail.com>
2023-02-13 19:46:25 +0100

    building

--- Ecommerce-clone/ecommerce-store/src/App.js
+++ Ecommerce-clone/ecommerce-store/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Signin from './components/';
 function App() {
   return (
-    <div className="App">
-    <h1>Lets start</h1>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
