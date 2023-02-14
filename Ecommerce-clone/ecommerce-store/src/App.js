e60b567f9cde5bc30ab823a8f4c57e17872e1e5d
Magnus Fröbom <frobom.magnus@gmail.com>
2023-02-14 06:02:19 +0100

    Added auth

--- Ecommerce-clone/ecommerce-store/src/App.js
+++ Ecommerce-clone/ecommerce-store/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./Home";
-import Signin from './components/';
+import Home from './components/Home';
+import Header from "./components/Header";
+import Login from "./components/Loginr";
+
+
 function App() {
   return (
     <div className="app">
@@ -16,4 +19,27 @@ function App() {
     </div>
   );
 }
+
+useEffect(() => {
+  // Runs once.
+
+  auth.onAuthStateChanged((authUser) => {
+    console.log("THE USER IS >>> ", authUser);
+
+    if (authUser) {
+      // User login
+
+      dispatch({
+        type: "SET_USER",
+        user: authUser,
+      });
+    } else {
+      // user logged out
+      dispatch({
+        type: "SET_USER",
+        user: null,
+      });
+    }
+  });
+}, []);
 export default App;
