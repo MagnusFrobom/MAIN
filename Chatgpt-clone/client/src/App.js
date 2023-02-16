c374c323c87601cfd8fbf4871fe561d3d6bd3cf5
Magnus Fröbom <frobom.magnus@gmail.com>
2023-02-16 21:57:12 +0100

    Starting Backend

--- Chatgpt-clone/client/src/App.js
+++ Chatgpt-clone/client/src/App.js
@@ -14,7 +14,24 @@ function App() {
           <div className="chat-message">
             <div className="chat-message-center">
               <div className="avatar">
-
+                <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx={50} cy={50} r={50} fill="#4C4C4C" />
+                  <rect x={20} y={35} width={60} height={35} rx={5} ry={5} fill="#4C4C4C" />
+                  <ellipse cx={50} cy={35} rx={30} ry={20} fill="#4C4C4C" />
+                  <path fill="#F8F8F8" d="m50 65 10 10H40z" />
+                  <text
+                    x={50}
+                    y={50}
+                    textAnchor="middle"
+                    alignmentBaseline="middle"
+                    fontSize={35}
+                    fontWeight="bold"
+                    fontFamily="sans-serif"
+                    fill="#FFF"
+                  >
+                    {"YOU"}
+                  </text>
+                </svg>
               </div>
               <div className="message">
                 Hi there!
@@ -25,7 +42,22 @@ function App() {
           <div className="chat-message aibot">
             <div className="chat-message-center">
               <div className="avatar aibot">
-
+                <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx={50} cy={50} r={50} fill="#4C4C4C" />
+                  <rect x={25} y={25} width={50} height={50} rx={5} ry={5} fill="#F8F8F8" />
+                  <path fill="#F8F8F8" d="m50 10 40 70H10z" />
+                  <text
+                    x={50}
+                    y={50}
+                    textAnchor="middle"
+                    alignmentBaseline="middle"
+                    fontSize={45}
+                    fontWeight="bold"
+                    fontFamily="sans-serif"
+                  >
+                    {"AI"}
+                  </text>
+                </svg>
               </div>
               <div className="message">
                 I'm your AI!
