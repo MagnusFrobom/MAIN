16050fea8b041a71f5849317d02c1bd1404b4ff6
Magnus Fröbom <frobom.magnus@gmail.com>
2023-02-18 12:10:58 +0100

    New button design

--- Chatgpt-clone/client/src/App.js
+++ Chatgpt-clone/client/src/App.js
@@ -47,22 +47,7 @@ function App() {
           <div className="chat-message chatgpt">
             <div className="chat-message-center">
               <div className="avatar chatgpt">
-                <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx={50} cy={50} r={50} fill="#4C4C4C" />
-                  <rect x={25} y={25} width={50} height={50} rx={5} ry={5} fill="#F8F8F8" />
-                  <path fill="#F8F8F8" d="m50 10 40 70H10z" />
-                  <text
-                    x={50}
-                    y={50}
-                    textAnchor="middle"
-                    alignmentBaseline="middle"
-                    fontSize={45}
-                    fontWeight="bold"
-                    fontFamily="sans-serif"
-                  >
-                    {"AI"}
-                  </text>
-                </svg>
+                
               </div>
               <div className="message">
                 I'm your AI!
@@ -114,7 +99,6 @@ const ChatMessage = ({ message }) => {
           </svg>
           }
           </div>
-
         <div className="message">
           Hi there!
         </div>
@@ -125,3 +109,23 @@ const ChatMessage = ({ message }) => {
 
 
 export default App;
+
+
+// AI SVG LOGO
+/* 
+<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <circle cx={50} cy={50} r={50} fill="#4C4C4C" />
+  <rect x={25} y={25} width={50} height={50} rx={5} ry={5} fill="#F8F8F8" />
+  <path fill="#F8F8F8" d="m50 10 40 70H10z" />
+  <text
+    x={50}
+    y={50}
+    textAnchor="middle"
+    alignmentBaseline="middle"
+    fontSize={45}
+    fontWeight="bold"
+    fontFamily="sans-serif"
+  >
+    {"AI"}
+  </text>
+</svg> */
