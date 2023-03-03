2c76941e612c1818d64b502264eed80eeb5fc7b3
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-03 10:18:00 +0100

    Migration + Auth

--- LiveChat-T3-stack/livechat-t3/src/server/auth.ts
+++ LiveChat-T3-stack/livechat-t3/src/server/auth.ts
@@ -4,7 +4,6 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "livechat/env.mjs";
 import { prisma } from "livechat/server/db";
@@ -47,10 +46,7 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+
     /**
      * ...add more providers here.
      *
@@ -61,6 +57,7 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  secret: process.env.NEXTAUTH_SECRET
 };
 
 /**
