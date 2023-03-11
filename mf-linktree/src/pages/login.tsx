6678c8eb1ad29d0abd0f94e8dc80b6b88dc84172
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-11 08:44:41 +0100

    Personal Linktree NextJS

---
+++ mf-linktree/src/pages/login.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react"
+
+export default function Login() {
+        const [email, setEmail] = useState<string | undefined>();
+        const [password, setPassword] = useState<string | undefined>();
+    return <div>Yoosaaatt</div>
+}
